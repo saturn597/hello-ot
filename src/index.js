d9aaece2a0a5c60d1976592bc2f4f47fa5ac82bf
Saturn597 <saturn5971@gmail.com>
2021-06-09 11:05:22 -0400

    Board now starts with pieces on it

--- src/index.js
+++ src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    const squares = Array(64).fill('empty');
+    squares[27] =  'white';
+    squares[28] = 'black';
+    squares[35] = 'black';
+    squares[36] = 'white';
+    this.state = {
+      squares,
+    };
+  }
+
   render() {
-    let squares = Array(64).fill(null).map((_, i) => 
-      <div key={i} className="square"></div>);
+    const squares = this.state.squares.map((sq, i) =>
+      <Square status={sq} key={i}></Square>
+    );
     return <div id="board">{squares}</div>;
   }
+}
+
+function Square(props) {
+  return (
+    <button className="square">
+      <div className={props.status}></div>
+    </button>
+  );
 }
 
 ReactDOM.render(
